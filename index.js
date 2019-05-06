22533e67d9958035b0cd1c187a9761fd177e6710
Ryan Bravo <rrbravo2@gmail.com>
2019-05-06 08:15:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var price = Math.floor(100*(Math.random()));
-  var itemObj = {[item]: price};
-  cart.push(itemObj);
-  console.log(`${item} has been added to your cart.`);
-  return cart;
+ 
 }
 
 function viewCart() {
