6c2183c3e44d980d28d63578f01cca46490e5dc4
Ryan Bravo <rrbravo2@gmail.com>
2019-05-06 08:14:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,19 @@ function addToCart(item) {
 }
 
 function viewCart() {
- 
+ if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  } else if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  } else {
+    var myString = "In your cart, you have "
+    for(var i = 0; i < cart.length - 1; i++) {
+      myString = myString + `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    }
+    return myString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+  }
 }
 
 function total() {
