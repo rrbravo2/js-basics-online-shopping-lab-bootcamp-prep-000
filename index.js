9cb5b0322ebf02375abf57b4e190279cc3396f2c
Ryan Bravo <rrbravo2@gmail.com>
2019-05-06 08:11:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,12 +24,10 @@ function viewCart() {
 function total() {
   var pricePerItemInCart = [];
   var totalPrice = 0
-  for(var i = 0; i < cart.length; i++)
-  {
+  for(var i = 0; i < cart.length; i++){
     var itemPrice = cart[i][Object.keys(cart[i])]
     pricePerItemInCart.push(itemPrice)
-    
-    for (var j =0; j<pricePerItemInCart.length; j++)
+    for (var j = 0; j < pricePerItemInCart.length; j++)
     {
       totalPrice = pricePerItemInCart[j]+=totalPrice;
     }
