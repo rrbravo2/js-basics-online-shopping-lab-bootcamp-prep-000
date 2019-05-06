cad47b271fd211a43c64035c3e398bb4b5d54ba3
Ryan Bravo <rrbravo2@gmail.com>
2019-05-06 08:26:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,7 +55,12 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
- 
+ if (cardNumber === undefined) {
+    console.log('Sorry, we don\'t have a credit card on file for you.')
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  }
+  cart = [];
 }
 
 
