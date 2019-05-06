1e577d8f95977590128e5c34196f48d85cbc2e3c
Ryan Bravo <rrbravo2@gmail.com>
2019-05-06 07:57:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,27 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 100) + 1;
+ var item_object = {};
+ item_object[item] = price
+ cart.push(item_object)
+ console.log(item + "has been added to your cart.");
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+ if(cart.length === 0) {
+   console.log("Your shopping cart is empty.");
+ } else {
+   var items = [];
+   for(var i = 0; i < cart.length; i++) {
+     for(var item in cart[i]) {
+       items.push(items + " at $" + cart[i][item])
+         
+       }
+     }
+     console.log("In your cart, you have " + items.join(", ") + ".");
+ }
 }
 
 function total() {
