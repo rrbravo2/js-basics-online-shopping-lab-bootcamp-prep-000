fc26bae37c0b0e17fec6b50aa518b0b4bfcf4e93
Ryan Bravo <rrbravo2@gmail.com>
2019-05-06 08:03:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,14 @@ function total() {
 }
 
 function removeFromCart(item) {
- 
+ for (var i = 0; i <cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1)
+      return cart
+    } 
+  }
+  console.log('That item is not in your cart.')
+  return cart
 }
 
 function placeOrder(cardNumber) {
